cc303da32465b825ee37a194cd59e0843c4b03af
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-21 10:57:59 +0700

    Set text during runtime

--- app/src/main/java/com/edts/happybirthday/MainActivity.kt
+++ app/src/main/java/com/edts/happybirthday/MainActivity.kt
@@ -2,11 +2,21 @@ package com.edts.happybirthday
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.edts.happybirthday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.setup()
+    }
+
+    private fun ActivityMainBinding.setup() {
+        to = "Sam"
+        from = "Emma"
     }
 }
